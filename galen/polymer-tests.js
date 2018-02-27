4a6e2569c05a5291e35b7df6271cef3301c7d90b
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2018-02-27 16:39:28 -0500

    Handle errors better

--- galen/polymer-tests.js
+++ galen/polymer-tests.js
@@ -1,10 +1,18 @@
-/* global PolymerPage readFile */
+/* global PolymerPage readFile takeScreenshot */
 /* eslint no-invalid-this: 0 */
 'use strict';
 
 load('polymer-page.js');
 
 function polymerTests(browsers, runTests) {
+	function loggerWrapper(report, driver, cb) {
+		try {
+			cb(report);
+		} catch (e) {
+			report.error(e.toString() + (e.stack || e.message || e.name)).withAttachment('Screenshot', takeScreenshot(driver));
+		}
+	}
+
 	var dumpsDir = System.getProperty('d2l.galen.utils.dumps');
 	Object.keys(browsers).forEach(function(browserName) {
 		var factory = browsers[browserName];
@@ -33,45 +41,45 @@ function polymerTests(browsers, runTests) {
 				// Navigate, size browser, then checkLayout/dumpPage or call callback
 				function innerTest(name, opts, cb) {
 					logged(name, function(report) {
-						driver.get(opts.endpoint);
-						report.info('GET ' + opts.endpoint);
-						var size = opts.size || factory.settings.size;
-						size && resize(driver, size);
-						size && report.info('Resize: ' + size);
-						polymerPage.waitForIt();
+						loggerWrapper(report, driver, function(report) {
+							driver.get(opts.endpoint);
+							report.info('GET ' + opts.endpoint);
+							var size = opts.size || factory.settings.size;
+							size && resize(driver, size);
+							size && report.info('Resize: ' + size);
+							polymerPage.waitForIt();
 
-						cb = cb || defaultCb;
-						var cloneOpts = {
-							name: name,
-							driver: driver,
-							report: report,
-							exportPath: dumpsDir && dumpsDir + '/' + name
-						};
-						Object.keys(opts).forEach(function(key) {
-							cloneOpts[key] = opts[key];
+							var runTest = 'function' === typeof opts.runTest ? opts.runTest : defaultCb;
+							cb = cb || runTest;
+							var cloneOpts = {
+								name: name,
+								driver: driver,
+								report: report,
+								exportPath: dumpsDir && dumpsDir + '/' + name
+							};
+							Object.keys(opts).forEach(function(key) {
+								cloneOpts[key] = opts[key];
+							});
+							cloneOpts.tags = [].concat(cloneOpts.tags || [], factory.settings.tags || []);
+							cb(cloneOpts, runTest.bind(null, cloneOpts));
 						});
-						cloneOpts.tags = [].concat(cloneOpts.tags || [], factory.settings.tags || []);
-						cb(cloneOpts, defaultCb.bind(null, cloneOpts));
 					});
 				}
 				innerTest.shadow = (hasShadow && !dumpsDir) ? innerTest : function() {};
 				innerTest.demo = dumpsDir ? innerTest : function() {};
 
-				try {
+				loggerWrapper(this.report, driver, function(report) {
 					polymerPage = new PolymerPage(driver);
 					runTests.call(this, innerTest, {
 						driver: driver,
 						polymerPage: polymerPage,
-						factory: factory
+						factory: factory,
+						report: report
 					});
-					var passed = this.report.fetchStatistic().getErrors() === 0;
-					factory.reportStatus(driver, passed);
-				} catch (e) {
-					factory.reportStatus(driver, false);
-					throw e;
-				} finally {
-					driver.quit();
-				}
+				});
+				var passed = this.report.fetchStatistic().getErrors() === 0;
+				factory.reportStatus(driver, passed);
+				driver.quit();
 			});
 		});
 	});
